9a93c91e5cc51058cf98a9ee566d7dc0c4df75cf
ozwaldorf <self@ossian.dev>
2022-06-15 23:26:38 -0400

    chore: cargo fmt

--- ic/emporium/src/ledger.rs
+++ ic/emporium/src/ledger.rs
@@ -1,37 +1,37 @@
 use ic_kit::{
-  candid::{CandidType, Deserialize},
-  Principal,
+    candid::{CandidType, Deserialize},
+    Principal,
 };
 use std::cell::RefCell;
 use std::collections::HashMap;
 
 #[derive(Clone, Deserialize, CandidType)]
 pub struct DailyData {
-  pub last_timestamp: u64,
-  pub streak: u64,
+    pub last_timestamp: u64,
+    pub streak: u64,
 }
 
 #[derive(Clone, Deserialize, CandidType)]
 pub struct WorkData {
-  pub last_timestamp: u64,
-  pub streak: u64,
+    pub last_timestamp: u64,
+    pub streak: u64,
 }
 
 #[derive(Clone, Deserialize, CandidType)]
 pub struct User {
-  pub discord_id: String,
-  pub principal: Principal,
-  pub daily: DailyData,
-  pub work: WorkData,
-  pub coins: u64,
+    pub discord_id: String,
+    pub principal: Principal,
+    pub daily: DailyData,
+    pub work: WorkData,
+    pub coins: u64,
 }
 
 #[derive(Clone, Deserialize, CandidType)]
 pub struct Ledger {
-  pub total_users: u64,
-  pub ft_canister: Option<Principal>,
-  pub nft_canister: Option<Principal>,
-  pub users: HashMap<String, User>,
+    pub total_users: u64,
+    pub ft_canister: Option<Principal>,
+    pub nft_canister: Option<Principal>,
+    pub users: HashMap<String, User>,
 }
 
 thread_local! {
@@ -44,9 +44,9 @@ thread_local! {
 }
 
 pub fn _with<T, F: FnOnce(&Ledger) -> T>(f: F) -> T {
-  LEDGER.with(|ledger| f(&ledger.borrow()))
+    LEDGER.with(|ledger| f(&ledger.borrow()))
 }
 
 pub fn with_mut<T, F: FnOnce(&mut Ledger) -> T>(f: F) -> T {
-  LEDGER.with(|ledger| f(&mut ledger.borrow_mut()))
+    LEDGER.with(|ledger| f(&mut ledger.borrow_mut()))
 }
