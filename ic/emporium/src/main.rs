af0d71929eb0322d8869ecb769842942c9790154
ozwaldorf <self@ossian.dev>
2022-06-15 23:25:26 -0400

    feat: initial daily logic

--- ic/emporium/src/main.rs
+++ ic/emporium/src/main.rs
@@ -1,65 +1,16 @@
 use compile_time_run::run_command_str;
 use ic_kit::{
-  candid::{candid_method, encode_args, CandidType, Deserialize, Nat},
+  candid::{candid_method, CandidType, Deserialize},
   ic,
   interfaces::{
-    management::{self, CanisterStatus, CanisterStatusResponse, WithCanisterId},
+    management::{CanisterStatus, WithCanisterId},
     Method,
   },
   macros::*,
   Principal,
 };
-use std::cell::RefCell;
-use std::collections::HashMap;
 
-mod ledger {
-  use super::*;
-  pub fn _with<T, F: FnOnce(&Ledger) -> T>(f: F) -> T {
-    LEDGER.with(|ledger| f(&ledger.borrow()))
-  }
-
-  pub fn with_mut<T, F: FnOnce(&mut Ledger) -> T>(f: F) -> T {
-    LEDGER.with(|ledger| f(&mut ledger.borrow_mut()))
-  }
-
-  #[derive(Clone, Deserialize, CandidType)]
-  pub struct DailyData {
-    pub last_timestamp: u64,
-    pub streak: u64,
-  }
-
-  #[derive(Clone, Deserialize, CandidType)]
-  pub struct WorkData {
-    pub last_timestamp: u64,
-    pub streak: u64,
-  }
-
-  #[derive(Clone, Deserialize, CandidType)]
-  pub struct User {
-    pub discord_id: String,
-    pub principal: Principal,
-    pub daily: DailyData,
-    pub work: WorkData,
-    pub coins: u64,
-  }
-
-  #[derive(Clone, Deserialize, CandidType)]
-  pub struct Ledger {
-    pub total_users: u64,
-    pub ft_canister: Option<Principal>,
-    pub nft_canister: Option<Principal>,
-    pub users: HashMap<String, User>,
-  }
-
-  thread_local! {
-    static LEDGER: RefCell<Ledger> = RefCell::new(Ledger {
-      total_users: 0,
-      ft_canister: None,
-      nft_canister: None,
-      users: HashMap::new(),
-    });
-  }
-}
+mod ledger;
 
 #[derive(Clone, Deserialize, CandidType)]
 pub struct InitArgs {
@@ -117,7 +68,7 @@ fn daily(discord_user: String) -> Result<(), String> {
 ///
 /// To let the canister call the `aaaaa-aa` Management API `canister_status`,
 /// the canister needs to be a controller of itself.
-async fn is_controller(principal: &Principal) -> Result<(), String> {
+async fn _is_controller() -> Result<(), String> {
   let caller = ic::caller();
   let self_id = ic::id();
 
